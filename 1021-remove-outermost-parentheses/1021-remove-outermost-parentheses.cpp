edbd1eaca581e05b1c78633c17c9d7f8fce91bc3
Ranjan Yadav <yranjan781@gmail.com>
2026-06-28 16:37:30 +0530

    Time: 0 ms (100%), Space: 8.8 MB (80.81%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string result = "";  
+        // Initialize nesting level counter
+        int level = 0;       
+
+        // Traverse the string
+        for (char ch : s) {
+            // If we encounter '(', increase the level
+            if (ch == '(') {
+                // If we're inside a primitive, add '(' to result
+                if (level >= 1) result += ch;
+                // Increase the nesting level for '('
+                level++;  
+            } 
+            // If we encounter ')', decrease the level
+            else if (ch == ')') {
+                // Decrease the nesting level for ')'
+                level--;  
+                // If we're inside a primitive, add ')' to result
+                if (level > 0) result += ch;
+            }
+        }
+
+        // Return the final result after removing the outer parentheses
+        return result;
+    }
+};
