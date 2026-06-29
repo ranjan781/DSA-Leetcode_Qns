c6285bb666226d4170d2b5ac0f0df2b2cb9dee30
Ranjan Yadav <yranjan781@gmail.com>
2026-06-29 16:08:56 +0530

    Update update_readme.py

--- scripts/update_readme.py
+++ scripts/update_readme.py
@@ -1,1 +1,70 @@
 
+from pathlib import Path
+
+from parser import scan_repository
+from generator import (
+    generate_stats,
+    generate_progress,
+    generate_recent,
+    generate_languages,
+    generate_date,
+)
+from utils import replace_section
+
+
+README = Path("README.md")
+
+
+def main():
+
+    problems = scan_repository()
+
+    if not README.exists():
+        print("README.md not found.")
+        return
+
+    text = README.read_text(encoding="utf-8")
+
+    text = replace_section(
+        text,
+        "<!-- AUTO_STATS_START -->",
+        "<!-- AUTO_STATS_END -->",
+        generate_stats(problems),
+    )
+
+    text = replace_section(
+        text,
+        "<!-- AUTO_PROGRESS_START -->",
+        "<!-- AUTO_PROGRESS_END -->",
+        generate_progress(problems),
+    )
+
+    text = replace_section(
+        text,
+        "<!-- AUTO_RECENT_START -->",
+        "<!-- AUTO_RECENT_END -->",
+        generate_recent(problems),
+    )
+
+    text = replace_section(
+        text,
+        "<!-- AUTO_LANG_START -->",
+        "<!-- AUTO_LANG_END -->",
+        generate_languages(problems),
+    )
+
+    text = replace_section(
+        text,
+        "<!-- AUTO_DATE_START -->",
+        "<!-- AUTO_DATE_END -->",
+        generate_date(),
+    )
+
+    README.write_text(text, encoding="utf-8")
+
+    print(f"README Updated Successfully!")
+    print(f"Problems Found : {len(problems)}")
+
+
+if __name__ == "__main__":
+    main()
