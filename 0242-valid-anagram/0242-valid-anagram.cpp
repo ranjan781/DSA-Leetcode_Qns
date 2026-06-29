23b00371b54ccb277100642a90b864ad57036ed1
Ranjan Yadav <yranjan781@gmail.com>
2026-06-29 22:56:19 +0530

    Time: 4 ms (34.96%), Space: 9.7 MB (49.43%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,21 +1,41 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length()==t.length()){
-            sort(s.begin(),s.end());
-            sort(t.begin(),t.end());
-            int i=0;
-            int j=0;
-            while(i<=s.length()){
-                if(s[i]==t[j]){
-                    i++;
-                    j++;
-                }else{
-                    return false;
-                }
-            }
-            return true;
+        if(s.length()!=t.length()){
+            return false;
         }
-        return false;
+        int freq[26]={0};
+        for(int i=0;i<s.length();i++){ //counting no. of each characters
+            freq[s[i]-'a']++;
+        }
+        for(int i=0;i<t.length();i++){ //counting no. of each characters
+            freq[t[i]-'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(freq[i]!=0){
+                return false;
+            }
+        }
+        return true;
     }
 };
+
+
+
+// if(s.length()==t.length()){
+//             sort(s.begin(),s.end());
+//             sort(t.begin(),t.end());
+//             int i=0;
+//             int j=0;
+//             while(i<=s.length()){
+//                 if(s[i]==t[j]){
+//                     i++;
+//                     j++;
+//                 }else{
+//                     return false;
+//                 }
+//             }
+//             return true;
+//         }
+//         return false;
+//     }
