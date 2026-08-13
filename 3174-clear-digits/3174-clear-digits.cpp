e1155b22a6ef67a45e318a700f5c4b29656fb1c2
Ranjan Yadav <yranjan781@gmail.com>
2026-08-13 22:23:08 +0530

    Time: 4 ms (3.63%), Space: 8.3 MB (84.65%) - LeetHub

---
+++ 3174-clear-digits/3174-clear-digits.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+       string ans;
+
+        for (char ch : s) {
+            if (isdigit(ch)) {
+                if (!ans.empty()) {
+                    ans.pop_back();
+                }
+            } else {
+                ans.push_back(ch);
+            }
+        }
+
+        return ans; 
+    }
+};
